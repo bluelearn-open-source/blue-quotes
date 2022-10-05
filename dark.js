5fc708ddee31e2527ebb96069b780d92bcf2ac42
Shubham-Yadav <shubhamyadav7063@gmail.com>
2022-10-05 23:15:08 +0530

    added logo, fixed dark mode issue, navbar, cards alignment and text

--- dark.js
+++ dark.js
@@ -1,41 +1,49 @@
 
-let darkMode=false;
-var dark=document.getElementById("dark");
-var moon=document.getElementById("moon");
-var sun=document.getElementById("sun");
-var nav=document.getElementById("nav");
-var body=document.getElementById("body");
+let darkMode = false;
+var dark = document.getElementById("dark");
+var moon = document.getElementById("moon");
+var sun = document.getElementById("sun");
+var nav = document.getElementById("nav");
+var body = document.getElementById("body");
 
-dark.addEventListener('click',handleCLick)
+dark.addEventListener('click', handleCLick)
 
-function handleCLick(){
-    if(darkMode===false){
-        darkMode=true;
-        moon.style.visibility="hidden";
-        sun.style.visibility="visible";
-        dark.style.backgroundColor="white";
-       
-        body.style.backgroundColor="#414758"
-        nav.style.backgroundColor="#28292e"
-        // console.log(nav);
+function handleCLick() {
+    if (darkMode === false) {
+        darkMode = true;
+        moon.style.visibility = "hidden";
+        sun.style.visibility = "visible";
+        dark.style.backgroundColor = "white";
+        body.style.backgroundColor = "#414758"
+        nav.style.backgroundColor = "#28292e"
     }
-    else{
-        darkMode=false;
-        dark.style.backgroundColor="#2e2f30";
-        body.style.backgroundColor="#eee"
-        sun.style.visibility="hidden";
-        moon.style.visibility="visible";
-         nav.style.backgroundColor="#dcdee6";
-        
-    }
-   
+    else {
+        darkMode = false;
+        dark.style.backgroundColor = "#2e2f30";
+        body.style.backgroundColor = "#eee"
+        sun.style.visibility = "hidden";
+        moon.style.visibility = "visible";
+        nav.style.backgroundColor = "#dcdee6";
 
-    // console.log(darkMode);
+    }
 }
-
-
-
 // function handleClick(){
 //     darkMode=true
 //     console.log(darkMode)
 // }
+
+//  Code for sticky navbar
+window.onscroll = function() {myNav()};
+
+var navbar = document.getElementById("nav");
+
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myNav() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
