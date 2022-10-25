41ef38017854a487bd0e724a1cb42d4997adefff
Aman <Aman>
2022-10-25 16:10:13 +0530

    dark.js fixed

--- dark.js
+++ dark.js
@@ -1,48 +1,10 @@
-let darkMode=false;
-var dark=document.getElementById("dark");
-var moon=document.getElementById("moon");
-var sun=document.getElementById("sun");
-var nav=document.getElementById("nav");
-var body=document.getElementById("body");
-var logo=document.getElementById("logo")
-
-dark.addEventListener('click',handleCLick)
-
-function handleCLick(){
-    if(darkMode===false){
-        darkMode=true;
-        moon.style.visibility="hidden";
-        sun.style.visibility="visible";
-        dark.style.backgroundColor="white";
-       logo.style.color="white";
-        body.style.backgroundColor="rgb(40, 46, 65)"
-        nav.style.backgroundColor="#28292e"
-        // console.log(nav);
-    }
-    else{
-
-        darkMode=false;
-        logo.style.color="black";
-        dark.style.backgroundColor="#2e2f30";
-        body.style.backgroundColor="#eee"
-        sun.style.visibility="hidden";
-        moon.style.visibility="visible";
-         nav.style.backgroundColor="#dcdee6";
-        
-    }
-   
-
-    // console.log(darkMode);
-}
-
 let darkMode = false;
-
 var dark = document.getElementById("dark");
 var moon = document.getElementById("moon");
 var sun = document.getElementById("sun");
 var nav = document.getElementById("nav");
 var body = document.getElementById("body");
-let logo = document.getElementById("logo")
+var logo = document.getElementById("logo")
 
 dark.addEventListener('click', handleCLick)
 
@@ -52,24 +14,22 @@ function handleCLick() {
     moon.style.visibility = "hidden";
     sun.style.visibility = "visible";
     dark.style.backgroundColor = "white";
+    logo.style.color = "white";
     body.style.backgroundColor = "rgb(40, 46, 65)"
     nav.style.backgroundColor = "#28292e"
-    logo.src = "./assets/blueQuote_dark.svg"
+    // console.log(nav);
   }
   else {
     darkMode = false;
+    logo.style.color = "black";
     dark.style.backgroundColor = "#2e2f30";
     body.style.backgroundColor = "#eee"
     sun.style.visibility = "hidden";
     moon.style.visibility = "visible";
     nav.style.backgroundColor = "#dcdee6";
-    logo.src = "./assets/blueQuote_light.svg"
-    
+
   }
-}
-function handleClick(){
-    darkMode=true
-    console.log(darkMode)
+  // console.log(darkMode);
 }
 
 //  Code for sticky navbar
